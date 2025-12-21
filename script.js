32fdecf883353d233173dd7e92cea11fd2ee38bb
Hashir <hashirbinatif83@gmail.com>
2025-12-21 15:37:10 +0500

    a little responsive

--- script.js
+++ script.js
@@ -136,6 +136,32 @@ async function main() {
         console.log(currentSong.currentTime ,currentSong.duration);
 
         document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%"
+    })
+
+    // Add an event listener to the seekbar
+    document.querySelector(".seekbar").addEventListener("click", e => {
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100
+        document.querySelector(".circle").style.left = percent + "%"
+
+        currentSong.currentTime = (currentSong.duration * percent) / 100
+    })
+
+    // Add an event listener for hamburger
+
+    document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "0"
+        console.log('ok');
+        
+    })
+    
+    // Add an event listener for close button
+    
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "-120%"
+        console.log('okay');
+        
     })
 
 }
